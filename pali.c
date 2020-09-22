acb17cb3c087e31545d75c334357a4942586f133
CheyenneAtapour <cheyenneatapour@gmail.com>
2020-09-22 10:15:03 -0700

    Added a missing gmp_ in print statement.

--- pali.c
+++ pali.c
@@ -63,7 +63,7 @@ int main(int argc, char **argv)
 		printf("random number generated for vote: %d\n", random);
 		vote = paillier_plaintext_from_ui(random);
 		enc_vote = paillier_enc(NULL, pubKey, vote, paillier_get_rand_devurandom);
-		printf("encrypted vote: %Zd\n", enc_vote);
+		gmp_printf("encrypted vote: %Zd\n", enc_vote);
 		paillier_mul(pubKey, encrypted_sum, encrypted_sum, enc_vote);
 		gmp_printf("running encrypted sum: %Zd\n", encrypted_sum); 
 		printf("\n");
