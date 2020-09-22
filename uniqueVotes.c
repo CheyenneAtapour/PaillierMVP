da1496a928169abd2cd04e482701b0c80bd4e307
CheyenneAtapour <cheyenneatapour@gmail.com>
2020-09-22 01:03:33 -0700

    Added uniqueVotes to prove random encryption of votes.

--- uniqueVotes.c
+++ uniqueVotes.c
@@ -1,3 +1,8 @@
+/*
+	This program demonstrates that the same vote will appear different
+*/
+
+
 #include <stdio.h>
 #include <gmp.h>
 #include <paillier.h>
@@ -57,22 +62,37 @@ int main(int argc, char **argv)
 	decrypted2 = paillier_dec(NULL, pubKey, secKey, cipher2);
 	gmp_printf("Ciphertext decrypted: %Zd\n", decrypted2);
 
-	// Make 100 random valid votes 
+	// Make another vote of 2 and prove that it's different
+	// Create plaintext of 2	
+	paillier_plaintext_t* vote3;	
+	vote3 = paillier_plaintext_from_ui(2);	
+	gmp_printf("Plaintext created: %Zd\n", vote3);
 
-	// Print the result of the election
+	// Encrypt the plaintext of 2
+	paillier_ciphertext_t* cipher3;
+	cipher3 = paillier_enc(NULL, pubKey, vote3, paillier_get_rand_devurandom);
+	gmp_printf("Ciphertext created: %Zd\n", cipher3);
 
-
-	// Show that a vote can't be invalid invoking zkps
+	// Decrypt the plaintext of 2
+	paillier_plaintext_t* decrypted3;
+	decrypted3 = paillier_dec(NULL, pubKey, secKey, cipher3);
+	gmp_printf("Ciphertext decrypted: %Zd\n", decrypted3);
 
 	// Free memory
 	paillier_freepubkey(pubKey);
 	paillier_freeprvkey(secKey);
 	paillier_freeplaintext(vote);
 	paillier_freeplaintext(vote1);
+	paillier_freeplaintext(vote2);
+	paillier_freeplaintext(vote3);
 	paillier_freeplaintext(decrypted);
 	paillier_freeplaintext(decrypted1);
+	paillier_freeplaintext(decrypted2);
+	paillier_freeplaintext(decrypted3);
 	paillier_freeciphertext(cipher);
 	paillier_freeciphertext(cipher1);
+	paillier_freeciphertext(cipher2);
+	paillier_freeciphertext(cipher3);
 
 	return 0;
 }
