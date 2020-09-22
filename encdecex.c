8cc5f468daa0ce65e149ef79a8b8e0149350b4d4
CheyenneAtapour <cheyenneatapour@gmail.com>
2020-09-22 00:50:28 -0700

    Added encrypted double sum.

--- encdecex.c
+++ encdecex.c
@@ -12,39 +12,20 @@ int main(int argc, char **argv)
 
 	printf("Public key generated:\n%s\n", paillier_pubkey_to_hex(pubKey));
 
-	// Create plaintext of 0
+	// Create plaintext
 	paillier_plaintext_t* vote;	
 	vote = paillier_plaintext_from_ui(0);	
 	gmp_printf("Plaintext created: %Zd\n", vote);
 
-	// Encrypt the plaintext of 0
+	// Encrypt the plaintext
 	paillier_ciphertext_t* cipher;
 	cipher = paillier_enc(NULL, pubKey, vote, paillier_get_rand_devurandom);
 	gmp_printf("Ciphertext created: %Zd\n", cipher);
 
-	// Decrypt the ciphertext of 0
+	// Decrypt the ciphertext
 	paillier_plaintext_t* decrypted;
 	decrypted = paillier_dec(NULL, pubKey, secKey, cipher);
 	gmp_printf("Ciphertext decrypted: %Zd\n", decrypted);
-
-	// Create plaintext of 1
-
-	// Encrypt the plaintext of 1
-
-	// Decrypt the ciphertext of 1
-
-	// Create plaintext of 2
-
-	// Encrypt the plaintext of 2
-
-	// Decrypt the ciphertext of 2
-
-	// Make 100 random valid votes 
-
-	// Print the result of the election
-
-
-	// Show that a vote can't be invalid invoking zkps
 
 	// Free memory
 	paillier_freepubkey(pubKey);
